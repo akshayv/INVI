7ce3ec6719d6c1058983d5a968ad3a25f9a9b272
Raghav Ramesh <raghavramesh94@gmail.com>
2014-09-13 21:03:40 +0800

    refactor: separate functions

--- trunk/business/wifi/parser/WiFiParser.py
+++ trunk/business/wifi/parser/WiFiParser.py
@@ -1,25 +1,25 @@
 from integration.http.map.MapRetriever import MapRetriever
 from integration.terminal.CommandExecutor import CommandExecutor
-import subprocess
-import tempfile
-
-import os
 
 __author__ = 'raghav'
 
 class WiFiParser:
     @staticmethod
-    def parseWiFiAccessPoints(wifiJson):
+    def obtainWiFiAccessPoints(wifiJson):
         accessPoints = wifiJson["wifi"]
+        return accessPoints
 
-        # change this to iwlist scan
-        command = ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport','-sNUS']
-
+    @staticmethod
+    def obtainWiFiStrength(ssid):
+        command = ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport','-s'+ssid]
         output = CommandExecutor().executeCommand(command)
-        print output
+        return output
 
 if __name__ == "__main__":
     wifiParser = WiFiParser()
-    wifiJson = MapRetriever().retrieveData("DemoBuilding", 1)
-    accessPoints = wifiParser.parseWiFiAccessPoints(wifiJson)
-    print accessPoints
+    # wifiJson = MapRetriever().retrieveData("DemoBuilding", 1)
+    # accessPoints = wifiParser.obtainWiFiAccessPoints(wifiJson)
+    wifiStrength = wifiParser.obtainWiFiStrength("NUS")
+    print type(wifiStrength)
+    # print accessPoints
+    print wifiStrength
--- trunk/integration/terminal/CommandExecutor.py
+++ trunk/integration/terminal/CommandExecutor.py
@@ -5,5 +5,6 @@ __author__ = 'raghav'
 class CommandExecutor:
     @staticmethod
     def executeCommand(commandString):
+        # change to iwlist scan
         output = subprocess.Popen(commandString, stdout=subprocess.PIPE).communicate()[0]
         return output
