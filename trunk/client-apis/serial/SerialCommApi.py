0f1423131dac168abc1ee40499b01a26fbec5f76
akshay <akshay.irock@gmail.com>
2014-09-16 15:12:59 +0200

    Added tests and refactored

--- trunk/client-apis/serial/SerialCommApi.py
+++ trunk/client-apis/serial/SerialCommApi.py
@@ -1,10 +1,4 @@
-from threading import Thread
-from time import sleep
-from SerialQueueListener import SerialQueueListener
-from business.deadreckoning.PositionCalculator import PositionCalculator
-from business.deadreckoning.StepCounter import StepCounter
-from domain.deadreckoning.AccelerometerReading import AccelerometerReading
-from domain.deadreckoning.SensorReading import SensorReading
+from business.deadreckoning.SerialQueueListener import SerialQueueListener
 
 __author__ = 'akshay'
 
@@ -15,26 +9,3 @@ class SerialCommApi:
     def onSerialMessage(item):
         #TODO READ SERIAL DATA FROM PORT AND BATCH AND SEND
         SerialQueueListener.queue.put(item)
-
-if __name__ == '__main__':
-    api = SerialCommApi()
-    positionCalculator = PositionCalculator(0, 0, 0)
-    for i in range(1):
-        t = Thread(target=SerialQueueListener.listen)
-        t.daemon = True
-        t.start()
-
-    SerialQueueListener.queue.put(SensorReading(AccelerometerReading(0, 0, 0), 0, 0))
-    timeOffset = 100
-    for i in range(50):
-        if i % 2 == 0:
-            SerialQueueListener.queue.put(SensorReading(AccelerometerReading(0, 0, 13), 0, timeOffset))
-        elif i % 2 == 1:
-            SerialQueueListener.queue.put(SensorReading(AccelerometerReading(0, 0, 6), 0, timeOffset))
-        timeOffset += 100
-
-    SerialQueueListener.queue.join()
-
-    print StepCounter().getSteps()
-    print positionCalculator.getX()
-    print positionCalculator.getY()
