5815b429eabeb4210de0d3486fbf8567001f412c
Raghav Ramesh <raghavramesh94@gmail.com>
2014-09-23 12:19:04 +0800

    refactor: remove unwanted imports, rename methods, remove comments

--- trunk/business/wifi/LocationCalculator.py
+++ trunk/business/wifi/LocationCalculator.py
@@ -1,6 +1,5 @@
 from sympy import *
-from integration.http.map.MapRetriever import MapRetriever
-from integration.terminal.CommandExecutor import CommandExecutor
+from domain.wifi.AccessPoint import AccessPoint
 
 __author__ = 'raghav'
 
@@ -10,23 +9,21 @@ class LocationCalculator:
         topThree = sorted(apInfo, key=lambda k: k['ssid'], reverse=True)[:3]
         return topThree
 
-    def computeDistanceFromSSID(self, strength):
-        # obtain distance from strength
+    def computeDistanceFromSSID(self, ssid):
         A = -10 # Received signal in dBm at 1 metre - need to calibrate this - setting it at -10 for now
         n = 2.7 # Path loss component - Ranges from 2.7 to 4.3
-        distance = 10**((A - strength)/(10*n))
+        distance = 10**((A - ssid)/(10*n))
         return distance
 
     def computeLocation(self, coord_a, coord_b, coord_c):
-        # solve equations to return a range of points
         x, y, z = S('x y z'.split())
         equations = [
             Eq((x - coord_a['x'])**2 + (y - coord_a['y'])**2 + (z - coord_a['z'])**2, coord_a['distance']**3),
             Eq((x - coord_b['x'])**2 + (y - coord_b['y'])**2 + (z - coord_b['z'])**2, coord_b['distance']**2),
             Eq((x - coord_c['x'])**2 + (y - coord_c['y'])**2 + (z - coord_c['z'])**2, coord_c['distance']**2)
         ]
-        # print solve(equations, [x, y, z])
-        # print x, y
+        return solve(equations, [x, y, z])
+
 
 if __name__ == "__main__":
     wifiParser = LocationCalculator()
