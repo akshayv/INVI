2cf773423d32d101dcab7930f8fb90397a7e8296
Raghav Ramesh <raghavramesh94@gmail.com>
2014-09-30 12:43:25 +0800

    feat: main for wifi

--- trunk/business/wifi/LocationCalculator.py
+++ trunk/business/wifi/LocationCalculator.py
@@ -26,10 +26,53 @@ class LocationCalculator:
 
 
 if __name__ == "__main__":
-    wifiParser = LocationCalculator()
-    # for ap in accessPoints:
-    wifi = ""
-    for ap in ["NUS"]:
-        wifi = wifiParser.obtainWiFiStrength(ap)
-    wifiParser.computeLocation({ "x": 2, "y": 0, "z": 0, "distance": 2 },
-        { "x": 0, "y": 2, "z": 0, "distance": 2 }, { "x": -2, "y": 0, "z": 0, "distance": 2 })
+    location_calculator = LocationCalculator()
+    access_point = AccessPoint()
+
+    nearbyAP = access_point.getNearbyAccessPoints()
+    mapAP = access_point.getMapAccessPointsCoordinates()
+    filteredAP = []
+    for nap in nearbyAP:
+        for map in mapAP:
+            if access_point.checkIfBSSIDIsSame(nap['bssid'], map['bssid']):
+                filteredAP.append(nap)
+
+    # print filteredAP
+
+    topThree = location_calculator.computeTopThreeRSSI(filteredAP)
+    # print topThree
+    topBssidList = []
+    for ap in topThree:
+        topBssidList.append(ap['bssid'])
+    # print topBssidList
+
+    mapBssidList = access_point.getMapAccessPointsBSSIDs()
+    # print "top 3 bssids:", topBssidList
+    # for b in mapBssidList:
+    #     print "map bssids:", b['bssid']
+
+    coordList = []
+    # print topThree
+    # print mapBssidList
+
+
+    for bssid in topThree:
+        for b in mapBssidList:
+            # print b['bssid']
+            if AccessPoint.checkIfBSSIDIsSame(bssid['bssid'], b['bssid']):
+                # coordinates.append({"bssid")
+                coordList.append({ "nodeName": b["nodeName"], "bssid": b["bssid"], "nodeId": b["nodeId"], "rssi": bssid["rssi"] })
+    print coordList
+    print location_calculator.computeDistanceFromRSSI(-50)
+    # print coordList
+    for item in coordList:
+        item["distance"] = location_calculator.computeDistanceFromRSSI(item["rssi"])
+        coordinate = access_point.getCoordinate(item["nodeId"])
+        item["x"] = coordinate["x"]
+        item["y"] = coordinate["y"]
+
+    # print coordList
+    print location_calculator.computeLocation(coordList)
+
+    # print location_calculator.computeLocation([{'x': 0, "y": 2, "distance": 2}, {"x": 2, "y": 0, "distance": 2}, {"x": -2, "y": 0, "distance": 2}])
+
