af0887d598d1a6ef4b2ad939ef9fc14f273f4ae7
Raghav Ramesh <raghavramesh94@gmail.com>
2014-10-01 16:38:38 +0800

    refactor: comments; add building, level in getMapAccessPoints method

--- trunk/domain/wifi/AccessPoint.py
+++ trunk/domain/wifi/AccessPoint.py
@@ -8,17 +8,13 @@ __author__ = 'raghav'
 class AccessPoint:
     def filterRepeatingAccessPoints(self, ap_info):
         toBeRemoved = []
-        # print ap_info
         for ap1 in ap_info:
             for ap2 in ap_info:
                 if ap1['bssid'] != ap2['bssid']:
                     if self.checkIfBSSIDIsSame(ap1['bssid'], ap2['bssid']):
                         ap = ap1 if int(ap1['rssi']) < int(ap2['rssi']) else ap2
                         toBeRemoved.append(ap)
-        # print len(toBeRemoved)
-        # print len(ap_info)
         ap_info = [x for x in ap_info if x not in toBeRemoved]
-        # print len(ap_info)
         return ap_info
 
     def getNearbyAccessPoints(self):
@@ -26,22 +22,24 @@ class AccessPoint:
         if Platform().getOS() == "OS X":
             command = ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport', '-sNUS']
             output = CommandExecutor().executeCommandWithOutput(command).split('\n')
+
+            # Parse terminal output
             output.pop(-1) # Check for exception
             for networkInfo in output:
                 networkInfo = networkInfo.lstrip().rstrip().split(" ")
                 ap_info.append({ "bssid": networkInfo[1], "rssi": networkInfo[2] })
             ap_info.pop(0) # Check for exception
+
             # Filter similar APs
             ap_info = self.filterRepeatingAccessPoints(ap_info)
+
         elif Platform().getOS() == "Linux":
             pass
         return ap_info
 
-    def getMapAccessPoints(self):
-        wifiJson = MapRetriever().retrieveData("COM1", 2)
+    def getMapAccessPoints(self, building, level):
+        wifiJson = MapRetriever().retrieveData(building, level)
         accessPoints = wifiJson["wifi"]
-        # for item in accessPoints:
-        #     ssidList.append(item["nodeName"].encode("ascii", "ignore"))
         return accessPoints
 
     def getMapAccessPointsCoordinates(self):
@@ -57,9 +55,8 @@ class AccessPoint:
         for apc in accessPoints:
             accessPointsBSSIDs.append({ "bssid": apc["macAddr"], "nodeName": apc["nodeName"], "nodeId": apc["nodeId"] })
         return accessPointsBSSIDs
-
-    def getCoordinate(self, nodeId):
-        accessPoints = self.getMapAccessPoints()
+    def getCoordinate(self, building, level, nodeId):
+        accessPoints = self.getMapAccessPoints(building, level)
         coordinate = {}
         for ap in accessPoints:
             if ap['nodeId'] == nodeId:
@@ -67,7 +64,6 @@ class AccessPoint:
                 coordinate["x"] = ap["x"]
                 coordinate["y"] = ap["y"]
         return coordinate
-
 
     @staticmethod
     def checkIfBSSIDIsSame(bssid1, bssid2):
