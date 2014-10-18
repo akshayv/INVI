65b553c7abc8270fc06f1f6c4872247d14b3ffd8
akshay <akshay.irock@gmail.com>
2014-10-18 07:36:19 +0200

    Changed stride length and step calculation

--- trunk/domain/deadreckoning/AccelerometerReading.py
+++ trunk/domain/deadreckoning/AccelerometerReading.py
@@ -2,8 +2,8 @@ from math import sqrt
 
 __author__ = 'akshay'
 
-class AccelerometerReading:
 
+class AccelerometerReading:
     x = None
     y = None
     z = None
@@ -16,5 +16,11 @@ class AccelerometerReading:
     def __str__(self):
         return "( x = " + str(self.x) + ", y = " + str(self.y) + ", z = " + str(self.z) + " )"
 
-    def getAmplitude(self):
+    @staticmethod
+    def fromString(string):
+        parts = str(string).split(", ")
+        return AccelerometerReading(float(parts[0].replace("( x = ", "")), float(parts[1].replace("y = ", "")),
+                             float(parts[2].replace("z = ", "").replace(")", "")))
+
+    def amplitude(self):
         return sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
