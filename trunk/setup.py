91626c8db58442e87aec4edf1f48626d9d507584
Raghav Ramesh <raghavramesh94@gmail.com>
2014-10-24 17:46:35 +0800

    feat: add wifi to setup

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 
-setup(packages=['distutils', 'distutils.command'], requires=['pyttsx', 'sympy'],
+setup(packages=['distutils', 'distutils.command'], requires=['pyttsx', 'sympy', 'wifi'],
      )
