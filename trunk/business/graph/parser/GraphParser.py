c3fc7e89c30a7831e017c5afbbe3c1f7af12fe4a
Raghav Ramesh <raghavramesh94@gmail.com>
2014-11-14 00:15:31 +0800

    Added offset to certain points

--- trunk/business/graph/parser/GraphParser.py
+++ trunk/business/graph/parser/GraphParser.py
@@ -16,17 +16,22 @@ class GraphParser:
         for point in graphJson:
             if (building == "2" or building == "COM2") and level == 2 and str(point["nodeName"]).startswith(
                     "P") and str(point["nodeName"]) is not "P17":
-                pointObject = Point(int(point["nodeId"]) - 1, float(point["x"]), float(point["y"]), 15.0,
-                                    point["nodeName"])
+                pointObject = Point(int(point["nodeId"]) - 1, float(point["x"]), float(point["y"]),
+                                    point["nodeName"], 15.0)
             else:
-                pointObject = Point(int(point["nodeId"]) - 1, float(point["x"]), float(point["y"]), 0.0, point["nodeName"])
+                pointObject = Point(int(point["nodeId"]) - 1, float(point["x"]), float(point["y"]), point["nodeName"], 0.0)
             for linkedPointId in point["linkTo"].split(","):
                 # Map from 1-9 to 0-8
                 modifiedLinkedPointId = int(linkedPointId) - 1
                 linkedPoint = graphJson[modifiedLinkedPointId]
 
-                linkedPointObject = Point(modifiedLinkedPointId, float(linkedPoint["x"]), float(linkedPoint["y"]),
-                                          linkedPoint["nodeName"])
+                if (building == "2" or building == "COM2") and level == 2 and str(linkedPointObject["nodeName"]).startswith(
+                        "P") and str(linkedPointObject["nodeName"]) is not "P17":
+                    linkedPointObject = Point(modifiedLinkedPointId, float(linkedPoint["x"]), float(linkedPoint["y"]),
+                                          linkedPoint["nodeName"], 15.0)
+                else:
+                    linkedPointObject = Point(modifiedLinkedPointId, float(linkedPoint["x"]), float(linkedPoint["y"]),
+                                          linkedPoint["nodeName"], 0.0)
 
                 # The sample data was buggy. This is an unnecessary check
                 if not listGraph.isEdge(pointObject.getId(), linkedPointObject.getId()):
@@ -38,6 +43,6 @@ class GraphParser:
 
 if __name__ == "__main__":
     graphParser = GraphParser()
-    graphJson = MapRetriever().retrieveData("DemoBuilding", 1)
+    graphJson = MapRetriever().retrieveData(2, 2)
     listGraph = graphParser.parseGraph(graphJson)
     print listGraph
--- trunk/domain/graph/Point.py
+++ trunk/domain/graph/Point.py
@@ -8,7 +8,7 @@ class Point:
     __offset = 0.0
     __name = ""
 
-    def __init__(self, id, x=0.0, y=0.0, offset = 0.0, name=""):
+    def __init__(self, id, x=0.0, y=0.0, name="", offset = 0.0):
         self.__x = x
         self.__y = y
         self.__id = id
@@ -32,7 +32,7 @@ class Point:
 
     def __str__(self):
         return "( nodeId = " + str(self.getId()) + ", x = " + str(self.getX()) + ", y = " + str(self.getY()) \
-               + ", name = " + str(self.getName()) + " )"
+               + ", name = " + str(self.getName()) + ", offset =  " + str(self.getOffset()) + ")"
 
     def __repr__(self):
         return self.__str__()
@@ -43,7 +43,7 @@ class Point:
         stringPoint = stringPoint.replace("( ", "")
         array = stringPoint.split(", ")
         return Point(int(array[0].replace("nodeId = ", "")), float(array[1].replace("x = ", "")),
-                     float(array[2].replace("y = ", "")), array[3].replace("name = ", ""))
+                     float(array[2].replace("y = ", "")), array[3].replace("name = ", ""), 0.0)
 
 
 if __name__ == "__main__":
